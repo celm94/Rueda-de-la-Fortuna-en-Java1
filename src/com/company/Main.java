ccc381d5b4abca478203b9d36bbaef7a74dd7219
Célita Morales <cmoralesm37@miumg.edu.gt>
2021-03-10 02:56:03 -0600

    pequeñas ediciones. Hay que hacer la clase desde 0. No sé por qué no funciona el scanner. Ya me voy a dormir

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,36 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bienvenido a la Rueda de La Fortuna");
+        System.out.println("");
+        System.out.println("Advertencia: La exactitud de esta rueda de la fortuna está garantizada.");
+        System.out.println();
+        System.out.println("Estás jugando bajo tu propio riesgo.");
+        System.out.println("");
+
+
         String[] premios = new String[5];
-        premios[0] = "Unos pinches putazos";
-        premios[1] = "Un tarro de nutella";
-        premios[2]= "Un apollón de avioneta en el bondi";
-        premios[3]= "Una cocacola bien fría en la tienda de la esquina";
-        premios[4]= "Un beso de tu crush";
+        premios[0] = "que en exactamente 10 años, tu pack se filtrará por accidente y se volverá viral," +
+                "lo que te traerá muchas oportunidades como poder tener tu propio reality show," +
+                "lo cual rechazarás porque siempre dign@, nunca indign@.";
+        premios[1] = "que el 30 de Junio tocarán a tu puerta para fumigar, así que te irás " +
+                "a la tienda de la esquina por una cocacola. " +
+                "De repente entrará tu crush a la tienda, ignorándote " +
+                "por completo y le preguntará al tendero si hay Nutella.";
+        premios[2]= "en el 2,040 tú y tu familia huirán de Guatemala debido al debacle político y económico que ha provocado la dictadura" +
+                "de Neto Bran y para cubrir los gastos venderás tu cuerpo a una corporación para que realice experimentos." +
+                "Te transformarán en un cyborg, lo que extenderá tu tiempo de vida por 500 años más, de los cuales utilizarás los primeros" +
+                "20 prestando servicio militar para ayudar a establecer un nuevo orden mundial.";
+        premios[3]= "esta noche no dormirás porque necesitas estudiar para tu próximo parcial, pero pasarás la mitad de la noche" +
+                "viendo videos de bromas a gatos y stalkeando el instagram de tu crush.";
+        premios[4]= "ganaste un ¡CÓMODÍN! Felicidades, has ganado la oportunidad de elegir un fragmento de tu futuro. Escribe a continuación" +
+                "las cosas que quisieras que te sucedieran. Debes ser específico sobre en cuánto tiempo van a suceder. A disfrutar de la" +
+                "vida se ha dicho.";
         int random = (int) (Math.random()*5);
 
-        System.out.println("tu premio es " + premios[random] + " , jajajajajaja");
+        System.out.println("tu visión para un futuro próximo o lejano es que " + premios[random] + ", jajajajajaja");
     }
 }
